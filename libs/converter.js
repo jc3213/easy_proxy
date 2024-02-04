497d67272e94bcd7a7a93ccbc204603e2ae9a007
jc3213 <jc3213@users.noreply.github.com>
2024-02-04 13:06:09 +0800

    Delete libs/converter.js

--- libs/converter.js
+++
@@ -1,8 +0,0 @@
-function convertJsonToPAC(json) {
-    var pacscript = '';
-    json.proxies.forEach((proxy) => {
-        var regexp = `^(${json[proxy].replace(/[\s;\n]/g, '|').replace(/\./g, '\\.').replace(/\*/g, '.*')})$`;
-        pacscript += ` if (/${regexp}/i.test(host)) { return "${proxy}"; }`;
-    });
-    return `function FindProxyForURL(url, host) {${pacscript} return "DIRECT"; }`;
-}
