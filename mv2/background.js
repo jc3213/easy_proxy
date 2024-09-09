fb049006f244fab0592a9b4799ff1cf0184bfe94
jc3213 <jc3213@users.noreply.github.com>
2024-09-09 23:41:32 +0800

    fixed pac import

--- mv2/background.js
+++ mv2/background.js
@@ -141,7 +141,7 @@ function pacScriptConverter() {
     var tempo = '';
     easyStorage.proxies.forEach((proxy) => {
         if (easyStorage.pacs[proxy]) {
-            pac_script += '\n    ' + easyStorage[proxy].replace(/^[^{]*{/, '').replace(/(return "DIRECT"[^}]*)?}*[^}]*$/, '').trim();
+            pac_script += '\n    ' + easyStorage[proxy].replace(/^[^{]*{/, '').replace(/(return[^}]*)?}[^}]*$/, '').trim();
             return;
         }
         pac_script += convertRegexp(proxy, easyStorage[proxy]);
