a1b5d12304b85e711f074686937714d4f5d8359d
jc3213 <jc3213@users.noreply.github.com>
2024-09-20 07:21:23 +0800

    Update matchpattern.js

--- mv2/libs/matchpattern.js
+++ mv2/libs/matchpattern.js
@@ -1,77 +1,28 @@
 (() => {
-    const tlds = {
-        'aero': true,
-        'app': true,
-        'arpa': true,
-        'asia': true,
-        'biz': true,
-        'cat': true,
-        'co': true,
-        'com': true,
-        'coop': true,
-        'dev': true,
-        'edu': true,
-        'eu': true,
-        'gov': true,
-        'info': true,
-        'int': true,
-        'io': true,
-        'jobs': true,
-        'ltd': true,
-        'ltda': true,
-        'mil': true,
-        'mobi': true,
-        'museum': true,
-        'name': true,
-        'ne': true,
-        'net': true,
-        'org': true,
-        'post': true,
-        'pro': true,
-        'si': true,
-        'tel': true,
-        'test': true,
-        'travel': true,
-        'web': true,
-        'xxx': true,
-        'xyz': true
-    };
-
     const cache = {};
 
-    const tester = /((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/;
-
-    const ipv4 = (ipv4) => {
-        let [network, host, ...useless] = ipv4.split('.');
-        let result = network + '.' + host + '.*';
-        cache[ipv4] = result;
-        return result;
+    const ipv4 = (hostname) => {
+        return /((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(hostname);
     };
 
+    const isSLD = (hostname) => {
+        return /^[^\.]+\.[^\.]+$/.test(hostname);
+    }
+
     const make = (hostname) => {
-        if (tester.test(hostname)) {
-            return ipv4(hostname);
+        if (ipv4(hostname)) {
+            return hostname.replace(/\d+\.\d+$/, '*');
         }
 
-        let result;
-        let [tld, sld, sbd, ...useless] = hostname.split('.').reverse();
-
-        if (sld === undefined) {
-            result = hostname;
-        }
-        else if (sld in tlds) {
-            result = '*.' + sbd + '.' + sld + '.' + tld;
-        }
-        else {
-            result = '*.' + sld + '.' + tld;
+        if (isSLD(hostname)) {
+            return '*.' + hostname;
         }
 
-        cache[hostname] = result;
-        return result;
+        return hostname.replace(/^.+(\.[^.]+\.[^.]+)$/, '*$1');
     };
 
     const host = (hostname) => {
-        return cache[hostname] ?? make(hostname);
+        return cache[hostname] ??= make(hostname);
     };
     
     const url = (url) => {
