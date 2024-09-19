ef17b1c8430955a12037ff9894a0b5dc76f3df8e
jc3213 <jc3213@users.noreply.github.com>
2024-09-20 07:25:59 +0800

    fixed match pattern

--- mv2/libs/matchpattern.js
+++ mv2/libs/matchpattern.js
@@ -5,20 +5,12 @@
         return /((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(hostname);
     };
 
-    const isSLD = (hostname) => {
-        return /^[^\.]+\.[^\.]+$/.test(hostname);
-    }
-
     const make = (hostname) => {
         if (ipv4(hostname)) {
             return hostname.replace(/\d+\.\d+$/, '*');
         }
 
-        if (isSLD(hostname)) {
-            return '*.' + hostname;
-        }
-
-        return hostname.replace(/^.+(\.[^.]+\.[^.]+)$/, '*$1');
+        return hostname.replace(/^.*([^.]+\.[^.]+)$/, '*.$1');
     };
 
     const host = (hostname) => {
