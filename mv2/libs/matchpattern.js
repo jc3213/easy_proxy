774ed68b05012ff975e5a54db67b4142fb6754b6
jc3213 <jc3213@users.noreply.github.com>
2024-09-20 10:47:35 +0800

    oops

--- mv2/libs/matchpattern.js
+++ mv2/libs/matchpattern.js
@@ -1,16 +1,55 @@
 (() => {
+    const tlds = {
+        'aero': true,
+        'app': true,
+        'arpa': true,
+        'asia': true,
+        'biz': true,
+        'cat': true,
+        'co': true,
+        'com': true,
+        'coop': true,
+        'dev': true,
+        'edu': true,
+        'eu': true,
+        'gov': true,
+        'info': true,
+        'int': true,
+        'io': true,
+        'jobs': true,
+        'ltd': true,
+        'ltda': true,
+        'mil': true,
+        'mobi': true,
+        'museum': true,
+        'name': true,
+        'ne': true,
+        'net': true,
+        'org': true,
+        'post': true,
+        'pro': true,
+        'si': true,
+        'tel': true,
+        'test': true,
+        'travel': true,
+        'web': true,
+        'xxx': true,
+        'xyz': true
+    };
     const cache = {};
 
-    const ipv4 = (hostname) => {
-        return /((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(hostname);
-    };
-
     const make = (hostname) => {
-        if (ipv4(hostname)) {
+        if (/((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(hostname)) {
             return hostname.replace(/\d+\.\d+$/, '*');
         }
 
-        return hostname.replace(/^([^\.]+\.)*([^\.]+\.[^\.]+)$/, '*.$2');
+        var [host, sbd, sld, tld] = hostname.match(/(?:([^\.]+)\.)?([^\.]+)\.([^\.]+)$/);
+
+        if (!sbd || !tlds[sld]) {
+            return '*.' + sld + '.' + tld;
+        }
+
+        return '*.' + sbd + '.' + sld + '.' + tld;
     };
 
     const host = (hostname) => {
