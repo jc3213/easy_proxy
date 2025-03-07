e44f3d97c2dc2307dcf373722c3734a7960d94ff
jc3213 <jc3213@users.noreply.github.com>
2025-03-07 22:26:33 +0800

    移除 mv2/pages/common.js

--- mv2/pages/common.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('[i18n]').forEach((node) => {
-    node.textContent = chrome.i18n.getMessage(node.getAttribute('i18n'));
-});
-
-document.querySelectorAll('[i18n-tips]').forEach((node) => {
-    node.title = chrome.i18n.getMessage(node.getAttribute('i18n-tips'));
-});
