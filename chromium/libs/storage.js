67d7e139690d735f43d848e6bab69b735a8da966
jc3213 <jc3213@users.noreply.github.com>
2025-06-01 00:07:00 +0800

    简化storage.js

--- chromium/libs/storage.js
+++ chromium/libs/storage.js
@@ -1,86 +1,66 @@
 class Storage {
-    constructor (dbase, store) {
-        if (!dbase || typeof dbase !== 'string') {
+    constructor (database, store) {
+        if (!database || typeof database !== 'string') {
             throw new TypeError('parameter 1 must be a non-empty string!');
         }
         if (!store || typeof store !== 'string') {
             throw new TypeError('parameter 2 must be a non-empty string!');
         }
-        this.#dbase = dbase;
+        this.#database = database;
         this.#store = store;
         this.#db = new Promise((resolve, reject) => {
-            let request = indexedDB.open(dbase, 1);
+            let request = indexedDB.open(database, 1);
             request.onsuccess = () => resolve(request.result);
             request.onupgradeneeded = (event) => request.result.createObjectStore(store, { keyPath: 'key' });
             request.onerror = () => reject(request.error);
         });
     }
-    version = '0.1';
-    #dbase;
+    version = '1.0';
+    #database;
     #store;
     #db;
-    #trans (mode) {
-        return this.#db.then((db) => {
-            let transaction = db.transaction(this.#store, mode);
-            return transaction.objectStore(this.#store);
+    #transaction (callback) {
+        return new Promise(async (resolve, reject) => {
+            let db = await this.#db;
+            let transaction = db.transaction(this.#store, 'readwrite');
+            let store = transaction.objectStore(this.#store);
+            let request = callback(store);
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = () => reject(request.error);
         });
     }
     set (key, value) {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readwrite');
-            let request = store.put({ key, value });
-            request.onsuccess = () => resolve(value);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction((store) => store.put({ key, value }));
+    }
+    has (key) {
+        return this.#transaction((store) => store.get(key)).then((item) => item !== undefined);
     }
     get (key) {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readonly');
-            let request = store.get(key);
-            request.onsuccess = () => resolve(request.result?.value);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction((store) => store.get(key)).then((item) => item?.value);
     }
     delete (key) {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readwrite');
-            let request = store.delete(key);
-            request.onsuccess = () => resolve(true);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction('readwrite', (store) => store.delete(key));
     }
     entries () {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readonly');
-            let request = store.getAll();
-            request.onsuccess = () => resolve(request.result);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction((store) => store.getAll());
     }
     keys () {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readonly');
-            let request = store.getAllKeys();
-            request.onsuccess = () => resolve(request.result);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction((store) => store.getAllKeys());
     }
     values () {
         return this.entries().then((entries) => entries.map((item) => item.value));
     }
+    forEach (callback) {
+        return this.entries().then((entries) => entries.forEach(callback));
+    }
     clear () {
-        return new Promise(async (resolve, reject) => {
-            let store = await this.#trans('readwrite');
-            let request = store.clear();
-            request.onsuccess = () => resolve(true);
-            request.onerror = () => reject(request.error);
-        });
+        return this.#transaction('readwrite', (store) => store.clear());
     }
     flush () {
         return new Promise(async (resolve, reject) => {
             let db = await this.#db;
             db.close();
-            let request = indexedDB.deleteDatabase(this.#dbase);
+            let request = indexedDB.deleteDatabase(this.#database);
             request.onsuccess = () => resolve(true);
             request.onerror = () => reject(request.error);
         });
