a375d3f3b430fd4d8f0a766401e9746b36be7ad9
jc3213 <jc3213@users.noreply.github.com>
2025-12-03 19:58:30 +0800

    使用哈希查询提升大量规则的匹配性能

--- chromium/libs/easyproxy.js
+++ chromium/libs/easyproxy.js
@@ -1,7 +1,26 @@
 class EasyProxy {
-    #set = new Set();
-    #data = [];
-    #test = [];
+    static #instances = [];
+    static #caches = {};
+    static #etld = new Set([ 'co', 'com', 'net', 'org', 'gov', 'edu', 'ac', 'sch', 'ne', 'or', 'go' ]);
+    static #pacBody = `
+function FindProxyForURL(url, host) {
+    var origin = host;
+    while (true) {
+        var hit = RULES[host];
+        if (hit) {
+            RULES[origin] = hit;
+            return hit;
+        }
+        var dot = host.indexOf(".");
+        if (dot < 0) {
+            break;
+        }
+        host = host.substring(dot + 1);
+    }
+    return "DIRECT";
+}
+`;
+    #data = new Set();
     #empty = true;
     #global = false;
     #proxy;
@@ -13,13 +32,10 @@ class EasyProxy {
     }
 
     get data() {
-        return this.#data;
+        return [...this.#data];
     }
 
     set proxy(string) {
-        if (string !== 'DIRECT' && !/^(SOCKS5?|HTTPS?) ([^.]+\.)+[^.:]+(:\d{2,5})?$/.test(string)) {
-            throw new TypeError('Invalid proxy: expected "DIRECT" or a valid proxy (e.g., "HTTP 123.0.1.1:8080").');
-        }
         this.#proxy = string;
         this.#build();
     }
@@ -28,90 +44,41 @@ class EasyProxy {
     }
 
     get pacScript() {
-        return `function FindProxyForURL(url, host) {\n${this.#pacScript}\n${this.#global ? '' : '    return "DIRECT";\n'}}`;
+        return this.#empty
+            ? 'function FindProxyForURL(url, host) {\n    return "DIRECT";\n}'
+            : this.#global
+            ? `function FindProxyForURL(url, host) {\n    return "${this.#proxy}";\n}`
+            : `var RULES = {\n${this.#pacScript}\n};\n${EasyProxy.#pacBody}`;
     }
-
-    #update() {
-        this.#data = [...this.#set];
-        this.#test = this.#data.map((i) => `.${i}`);
-        this.#empty = this.#data.length === 0;
-        this.#global = this.#set.has('*');
-        this.#build();
-    }
-
-    #build() {
-        this.#pacScript = this.#empty ? ''
-            : this.#global ? `    return "${this.#proxy};"`
-            : `    if (${[...this.#data].map((i) => `dnsDomainIs(host, "${i}")`).join(' ||\n        ')}) {\n        return "${this.#proxy}";\n    }`;
-    }
-
-    new(arg) {
-        this.#set = new Set(Array.isArray(arg) ? arg : [arg]);
-        this.#update();
-    }
-
-    add(arg) {
-        Array.isArray(arg) ? arg.forEach((i) => this.#set.add(i)) : this.#set.add(arg);
-        this.#update();
-    }
-
-    delete(arg) {
-        Array.isArray(arg) ? arg.forEach((i) => this.#set.delete(i)) : this.#set.delete(arg);
-        this.#update();
-    }
-
-    clear() {
-        this.#set = new Set();
-        this.#data = [];
-        this.#test = [];
-        this.#empty = true;
-        this.#global = false;
-        this.#pacScript = '';
-    }
-
-    test(string) {
-        return !this.#empty && (this.#global || this.#set.has(string) || this.#test.some((i) => string.endsWith(i)));
-    }
-
-    static #instances = [];
-    static #caches = new Map();
-    static #tlds = new Set([
-        'aero', 'app', 'arpa', 'asia',
-        'biz',
-        'cat', 'co', 'com', 'coop',
-        'dev',
-        'edu', 'eu',
-        'gov',
-        'info', 'int', 'io',
-        'jobs',
-        'ltd', 'ltda',
-        'mil', 'mobi', 'museum',
-        'name', 'ne', 'net',
-        'org',
-        'post', 'pro',
-        'si',
-        'tel', 'test', 'travel',
-        'web',
-        'xxx', 'xyz'
-    ]);
 
     static get caches() {
         return EasyProxy.#caches;
     }
 
     static get pacScript() {
-        let pacScript = EasyProxy.#instances.map((i) => i.#pacScript).join('\n');
-        return `function FindProxyForURL(url, host) {${pacScript}\n    return "DIRECT";\n}`;
+        let rules = [];
+        for (let i of EasyProxy.#instances) {
+            if (i.#empty) {
+                continue;
+            }
+            if (i.#global) {
+                return `function FindProxyForURL(url, host) {\n    return "${i.#proxy}";\n}`;
+            }
+            rules.unshift(i.#pacScript);
+        }
+        return rules.length === 0
+            ? 'function FindProxyForURL(url, host) {\n    return "DIRECT";\n}'
+            : `var RULES = {\n${rules.join(",\n")}\n};\n${EasyProxy.#pacBody}`;
     }
 
-    static make(string) {
-        let rule = EasyProxy.#caches.get(string);
+    static make(host) {
+        let rule = EasyProxy.#caches[string];
         if (rule) {
             return rule;
         }
         let [, sbd, sld, tld] = string.match(/(?:([^.]+)\.)?([^.]+)\.([^.]+)$/);
-        rule = sbd && EasyProxy.#tlds.has(sld) ? `${sbd}.${sld}.${tld}` : `${sld}.${tld}`;
-        EasyProxy.#caches.set(string, rule);
+        rule = sbd && EasyProxy.#etld.has(sld) ? `${sbd}.${sld}.${tld}` : `${sld}.${tld}`;
+        EasyProxy.#caches[string] = rule;
         return rule;
     }
 
@@ -122,5 +89,59 @@ class EasyProxy {
     static delete(arg) {
         let remove = Array.isArray(arg) ? arg : [arg];
         EasyProxy.#instances = EasyProxy.#instances.filter((i) => !remove.includes(i.#proxy));
+    }
+
+    #update() {
+        this.#empty = this.#data.size === 0;
+        this.#global = this.#data.has('*');
+        this.#build();
+    }
+
+    #build() {
+        if (!this.#empty) {
+            this.#pacScript = this.data.map((i) => `    "${i}": "${this.#proxy}"`).join(',\n');
+        }
+    }
+
+    new(arg) {
+        this.#data = new Set(Array.isArray(arg) ? arg : [arg]);
+        this.#update();
+    }
+
+    add(arg) {
+        Array.isArray(arg) ? arg.forEach((i) => this.#data.add(i)) : this.#data.add(arg);
+        this.#update();
+    }
+
+    delete(arg) {
+        Array.isArray(arg) ? arg.forEach((i) => this.#data.delete(i)) : this.#data.delete(arg);
+        this.#update();
+    }
+
+    clear() {
+        this.#data = new Set();
+        this.#empty = true;
+        this.#global = false;
+        this.#pacScript = '';
+    }
+
+    test(host) {
+        if (this.#empty) {
+            return false;
+        }
+        if (this.#global) {
+            return true;
+        }
+        while (true) {
+            if (this.#data.has(host)) {
+                return true;
+            }
+            let dot = host.indexOf(".");
+            if (dot < 0) {
+                break;
+            }
+            host = host.substring(dot + 1);
+        }
+        return false;
     }
 }
