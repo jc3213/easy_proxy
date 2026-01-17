e6c3ab974ee69564ac724a282465054aaa5cb47c
jc3213 <jc3213@users.noreply.github.com>
2026-01-17 11:21:14 +0800

    撤回到Object

--- chromium/background.js
+++ chromium/background.js
@@ -64,9 +64,9 @@ function storageUpdated(response, json) {
 }
 
 function proxyQuery(response, tabId) {
-    let match = [];
-    let tempo = [];
-    let exclude = [];
+    let match = {};
+    let tempo = {};
+    let exclude = {};
     let { proxies, mode, preset } = easyStorage;
     let inspect = easyInspect[tabId];
     if (!inspect) {
@@ -74,14 +74,14 @@ function proxyQuery(response, tabId) {
         return;
     }
     for (let e of easyExclude.data) {
-        exclude.push([e, 'DIRECT']);
+        exclude[e] = 'DIRECT';
     }
     for (let proxy of proxies) {
         for (let m of easyMatch[proxy].data) {
-            match.push([m, proxy]);
+            match[m] = proxy;
         }
         for (let t of easyTempo[proxy].data) {
-            tempo.push([t, proxy]);
+            tempo[t] = proxy;
         }
     }
     let { rules, hosts, error } = inspect;
